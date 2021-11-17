c2eb53b9fdebe1a9321d6fd024b3e5b42e65bea7
Rawezhham176 <rawezhhame@gmail.com>
2021-11-17 12:24:02 +0100

    Initial Commit

---
+++ MyCard/MyCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyCardApp.swift
+//  MyCard
+//
+//  Created by rawezh on 17.11.21.
+//
+
+import SwiftUI
+
+@main
+struct MyCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
